477737db3f367a9b18a51a0df7f5ee7c8833ec74
Anthony Cavuoti <anthonycavuoti@gmail.com>
2020-06-09 15:20:43 +0000

    insall ejs and create blog idex route

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require("express");
 var app = express();
 
+//setting and getting info
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 app.get("/", (req, res) =>{
-	res.send("works");
+	res.redirect("/blog");
+})
+app.get("/blog", (req, res) => {
+	res.render("blog/index");
 })
 
 app.listen((process.env.PORT || 3000), process.env.IP, () => {
