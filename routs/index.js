eeeb8c3ddb898d589cfe6f55ffd9008b37be61d2
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2021-08-02 22:36:42 -0700

    Add Privacy polocy for game

--- routs/index.js
+++ routs/index.js
@@ -1,41 +1,47 @@
 //index routs
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var User = require("../models/User");
-var passport = require("passport");
+var User = require('../models/User');
+var passport = require('passport');
 
 //Sign up routs
-router.get("/register", (req, res) => {
-	res.render("register");
+router.get('/register', (req, res) => {
+  res.render('register');
 });
-router.post("/register", (req, res) => {
-	var newUser = new User({username: req.body.username, type: "user"});
-	User.register(newUser, req.body.password, (err, user) => {
-		if(err){
-			console.log(err.message);
-			return res.render("register");
-		}
-		else{
-			passport.authenticate("local")(req, res, () => {
-				res.redirect("/blog");
-			});
-		}
-	})
+router.post('/register', (req, res) => {
+  var newUser = new User({ username: req.body.username, type: 'user' });
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      console.log(err.message);
+      return res.render('register');
+    } else {
+      passport.authenticate('local')(req, res, () => {
+        res.redirect('/blog');
+      });
+    }
+  });
 });
 
 //login routs
-router.get("/login", (req, res) => {
-	res.render("login");
+router.get('/login', (req, res) => {
+  res.render('login');
 });
-router.post("/login", passport.authenticate("local", {
-	successRedirect: "/blog",
-	falureRedirect: "/login"
-}));
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/blog',
+    falureRedirect: '/login',
+  })
+);
 
 //logout route
-router.get("/logout", (req, res) => {
-	req.logout();
-	res.redirect("/blog");
-})
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/blog');
+});
+
+router.get('/chimpanzeeprivacypolicy', (req, res) => {
+  res.render('SpecticleGamesTerms/chimpanzee');
+});
 
 module.exports = router;
