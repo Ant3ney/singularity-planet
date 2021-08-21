9e31c1a3661a03f6031a960681b0e74793012735
Anthony Cavuoti <anthonycavuoti@gmail.com>
2021-08-21 14:12:56 -0700

    add pricing policy

--- routs/index.js
+++ routs/index.js
@@ -6,42 +6,46 @@ var passport = require('passport');
 
 //Sign up routs
 router.get('/register', (req, res) => {
-  res.render('register');
+	res.render('register');
 });
 router.post('/register', (req, res) => {
-  var newUser = new User({ username: req.body.username, type: 'user' });
-  User.register(newUser, req.body.password, (err, user) => {
-    if (err) {
-      console.log(err.message);
-      return res.render('register');
-    } else {
-      passport.authenticate('local')(req, res, () => {
-        res.redirect('/blog');
-      });
-    }
-  });
+	var newUser = new User({ username: req.body.username, type: 'user' });
+	User.register(newUser, req.body.password, (err, user) => {
+		if (err) {
+			console.log(err.message);
+			return res.render('register');
+		} else {
+			passport.authenticate('local')(req, res, () => {
+				res.redirect('/blog');
+			});
+		}
+	});
 });
 
 //login routs
 router.get('/login', (req, res) => {
-  res.render('login');
+	res.render('login');
 });
 router.post(
-  '/login',
-  passport.authenticate('local', {
-    successRedirect: '/blog',
-    falureRedirect: '/login',
-  })
+	'/login',
+	passport.authenticate('local', {
+		successRedirect: '/blog',
+		falureRedirect: '/login',
+	})
 );
 
 //logout route
 router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/blog');
+	req.logout();
+	res.redirect('/blog');
 });
 
 router.get('/chimpanzeeprivacypolicy', (req, res) => {
-  res.render('SpecticleGamesTerms/chimpanzee');
+	res.render('SpecticleGamesTerms/chimpanzee');
+});
+
+router.get('/businesspolicy/pricing', (req, res) => {
+	res.render('SingularityPolicy/price/index');
 });
 
 module.exports = router;
