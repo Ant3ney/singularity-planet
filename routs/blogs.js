8d623809b33e2afd0d81aa3dae03af4605eac62c
Anthony Cavuoti <anthonycavuoti@gmail.com>
2021-10-13 15:23:38 -0700

    Add safe guard code to view page

--- routs/blogs.js
+++ routs/blogs.js
@@ -1,83 +1,79 @@
 //Blog routs
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var middleware = require("../middleware/index.js");
-var mongoose = require("mongoose");
-var Blog = require("../models/Blog");
+var middleware = require('../middleware/index.js');
+var mongoose = require('mongoose');
+var Blog = require('../models/Blog');
 
-router.get("/", (req, res) => {
-	Blog.find({}, (err, allBlogs) => {
-		if(err){
-			console.log("Something went wrong");
-			console.log(err);
-		}
-		else{
-			res.render("blog/index", {blogs: allBlogs});
-		}
-	});
+router.get('/', (req, res) => {
+   Blog.find({}, (err, allBlogs) => {
+      if (err) {
+         console.log('Something went wrong');
+         console.log(err);
+      } else {
+         res.render('blog/index', { blogs: allBlogs });
+      }
+   });
 });
-router.get("/new", middleware.isAdmin, (req, res) => {
-	res.render("blog/new");
+router.get('/new', middleware.isAdmin, (req, res) => {
+   res.render('blog/new');
 });
-router.post("/", (req, res) => {
-	var postedBlog = middleware.assembleBlog(req);
-	Blog.create(postedBlog, (err, newBlog) => {
-		if(err){
-			console.log(err.message);
-		}
-		else{
-			res.redirect("/blog");
-		}
-	});
+router.post('/', (req, res) => {
+   var postedBlog = middleware.assembleBlog(req);
+   Blog.create(postedBlog, (err, newBlog) => {
+      if (err) {
+         console.log(err.message);
+      } else {
+         res.redirect('/blog');
+      }
+   });
 });
 //Show edit page
-router.get("/:blogId/edit", (req, res) => {
-	var blog_id = req.params.blogId;
-	
-	Blog.findById(blog_id, (err, blog) => {
-		if(err){
-			console.log(err.message);
-		}
-		else{
-			res.render("blog/edit", {blog: blog});
-		}
-	})
+router.get('/:blogId/edit', (req, res) => {
+   var blog_id = req.params.blogId;
+
+   Blog.findById(blog_id, (err, blog) => {
+      if (err) {
+         console.log(err.message);
+      } else {
+         res.render('blog/edit', { blog: blog });
+      }
+   });
 });
-router.put("/:blogId", (req, res) => {
-	var blog_id = req.params.blogId;
-	var updatedBlog =  middleware.assembleBlog(req);
-	
-	Blog.findByIdAndUpdate(blog_id, updatedBlog, (err, returnedBlog) => {
-		if(err){
-			console.log(err.message);
-		}
-		else{
-			res.redirect("/blog");
-		}
-	})
+router.put('/:blogId', (req, res) => {
+   var blog_id = req.params.blogId;
+   var updatedBlog = middleware.assembleBlog(req);
+
+   Blog.findByIdAndUpdate(blog_id, updatedBlog, (err, returnedBlog) => {
+      if (err) {
+         console.log(err.message);
+      } else {
+         res.redirect('/blog');
+      }
+   });
 });
-router.delete("/:blogId", (req, res) => {
-	var blog_id = req.params.blogId;
-	
-	Blog.findByIdAndRemove(blog_id, (err, deletedBlog) => {
-		if(err){
-			console.log(err.message);
-		}
-		else{
-			res.redirect("/blog");
-		}
-	})
+router.delete('/:blogId', (req, res) => {
+   var blog_id = req.params.blogId;
+
+   Blog.findByIdAndRemove(blog_id, (err, deletedBlog) => {
+      if (err) {
+         console.log(err.message);
+      } else {
+         res.redirect('/blog');
+      }
+   });
 });
-router.get("/:blogId", (req, res) => {
-	var blog_id = req.params.blogId;
-	Blog.findById(blog_id).populate("comments").exec((err, blog) => {
-		if(err){
-			console.log(err.message);
-		}
-		else{
-			res.render("blog/show", {blog: blog});
-		}
-	});
+router.get('/:blogId', (req, res) => {
+   var blog_id = req.params.blogId;
+   Blog.findById(blog_id)
+      .populate('comments')
+      .exec((err, blog) => {
+         if (err) {
+            console.log(err.message);
+         } else {
+            res.render('blog/show', { blog: blog });
+         }
+      });
 });
 
 module.exports = router;
